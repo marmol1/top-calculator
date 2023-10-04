79ffff561f7be7e59c59adec949ba46e2354fd68
Marmol <lucaguidotti93@gmail.com>
2023-10-04 13:29:07 -0300

    finished calculator

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function add(num1, num2) {
     return num1 + num2;
 }
 function subtract(num1, num2) {
-    return num1 + num2;
+    return num1 - num2;
 }
 function multiply(num1, num2) {
     return num1 * num2;
@@ -10,3 +10,79 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let $display = document.querySelector('.display');
+
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let displayValue = '';
+let result = '';
+function operate(operator,num1,num2) {
+    if(operator === '+'){
+        return add(num1,num2);
+    }else if(operator === '-') {
+        return subtract(num1,num2);
+    }else if(operator === 'x') {
+        return multiply(num1,num2);
+    }else {
+        return divide(num1,num2);
+    }
+}
+
+
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        if(button.classList.value === 'clear') {
+            firstNumber = '';
+            secondNumber = '';
+            display.innerHTML = '';
+            operator = '';
+            result = '';
+        }
+        if(result !== '') {
+            return;
+        }
+        if(button.classList.value === 'equal') {
+            if(firstNumber.innerHTML !== '' && display.innerHTML !== '') {
+                secondNumber = Number(display.innerHTML);
+                result = operate(operator, firstNumber,secondNumber)
+                display.innerHTML = result;
+            } else {
+                return;
+            }
+        }
+        
+        
+        if(button.classList.value === 'number'){
+            if(button.innerHTML === '.' && display.innerHTML.includes('.')) {
+                return;
+            }
+            
+            if(display.innerHTML === '') {
+                display.innerHTML = button.innerHTML;
+            } else {
+                display.innerHTML += button.innerHTML;
+            }
+        }
+
+        if(button.classList.value === 'operator') {
+            if(operator === '') {
+                operator = button.innerHTML;
+                if(display.innerHTML !== '') {
+                    firstNumber = Number(display.innerHTML);
+                    display.innerHTML = '';
+                }
+            } else {
+                return;
+            }
+        }
+        
+        
+        
+    })
+})
